e050302f44d580f52ec2bff6814ec86b9434ea1a
Nikolas Barkas <nikolas.barkas@outlook.com>
2020-05-28 18:07:37 -0400

    cleanups

--- umi_collapser.py
+++ umi_collapser.py
@@ -4,6 +4,9 @@ from statistics import (mode, StatisticsError)
 import os
 import sctools_imports
 
+BAM_CMATCH = 0 # M
+BAM_CREF_SKIP = 3 # N
+
 
 def get_read_family(record):
     tag_keys = ['XC', 'XM', 'gn']
@@ -12,7 +15,9 @@ def get_read_family(record):
 
 
 def umi_collapse_sorted_file(input_bam_filename, output_bam_filename, verbose=False, total_reads=None,
-                             debug=False, synthetic_read_prefix = "synthetic_read_"):
+                             debug=False, synthetic_read_prefix="synthetic_read_",
+                             family_temp_prefix="tmp/family_", save_collapsed_read=False,
+                             save_collapsed_read_prefix="tmp/family_collapsed_"):
     current_family = None
     current_family_size = 0
     family_index = 0
@@ -32,17 +37,22 @@ def umi_collapse_sorted_file(input_bam_filename, output_bam_filename, verbose=Fa
                 input_record_count = input_record_count + 1
                 current_read_family = get_read_family(input_record)
                 if current_family == current_read_family:
-                    # Continue outputting the same family
+                    # same family
                     current_family_size = current_family_size + 1
                     temp_bam.write(input_record)
                 else:
-                    # Next family starting copy and reset
+                    # next family
                     if temp_bam is not None:
                         temp_bam.close()
                         if current_family_size > 1:
-                            call_consensus(output_bam, temp_bam_filename,
-                                           new_read_name=f'{synthetic_read_prefix}{family_index}',
-                                           temp_sorted_file= f'{temp_bam_filename}.sorted.bam')
+                            new_read = call_consensus(temp_bam_filename,
+                                                      new_read_name=f'{synthetic_read_prefix}{family_index}',
+                                                      temp_sorted_file= f'{temp_bam_filename}.sorted.bam')
+                            if save_collapsed_read:
+                                with pysam.AlignmentFile(f'{save_collapsed_read_prefix}{family_index}', "wb",
+                                                         header=input_bam.header) as family_collapsed_file:
+                                    family_collapsed_file.write(new_read)
+                            output_bam.write(new_read)
                         else:
                             # copy read
                             with pysam.AlignmentFile(temp_bam_filename, "rb") as family_file:
@@ -51,11 +61,12 @@ def umi_collapse_sorted_file(input_bam_filename, output_bam_filename, verbose=Fa
                         if not debug:
                             os.remove(temp_bam_filename)
                     family_index = family_index + 1
-                    temp_bam_filename = f'tmp/family_{family_index}.bam'
+                    # bam file for all reads in family
+                    temp_bam_filename = f'{family_temp_prefix}{family_index}.bam'
                     temp_bam = pysam.AlignmentFile(temp_bam_filename, 'wb', header=input_bam.header)
-                    current_family_size = 1
                     current_family = current_read_family
                     temp_bam.write(input_record)
+                    current_family_size = 1
                 if show_progress_bar:
                     pbar.update(1)
             if temp_bam is not None:
@@ -68,12 +79,13 @@ def call_base(query_sequences, query_qualities):
         base_call = mode([x.upper() for x in query_sequences])
     except StatisticsError:
         base_call = 'N'
+    # TODO: Calculate quality (max observed)
+    quality = 'F'
+    return base_call, quality, 'M'
 
-    return base_call, 'F', 'M'
 
-
-def call_consensus(output_bam, temp_bam_filename, new_read_name=None, temp_sorted_file=None,
-                   max_depth=10000):
+def call_consensus(temp_bam_filename, new_read_name=None, temp_sorted_file=None,
+                   max_depth=10000, debug=False):
     assert temp_sorted_file is not None
     assert new_read_name is not None
 
@@ -90,8 +102,7 @@ def call_consensus(output_bam, temp_bam_filename, new_read_name=None, temp_sorte
     new_read_quality_list = []
     new_read_cigar_tuple_list = []
 
-    # M BAM_CMATCH 0
-    cigar_last = 0
+    cigar_last = BAM_CMATCH
     cigar_last_count = 0
 
     last_pileup_position = None
@@ -108,13 +119,12 @@ def call_consensus(output_bam, temp_bam_filename, new_read_name=None, temp_sorte
             position_delta = pos - last_pileup_position
             if position_delta > 1:
                 # We have a gap
-                if cigar_last == 3:
+                if cigar_last == BAM_CREF_SKIP:
                     cigar_last_count += 1
                 else:
                     new_read_cigar_tuple_list.append((cigar_last, cigar_last_count))
-                    # N BAM_CREF_SKIP 3
-                    new_read_cigar_tuple_list.append((3, position_delta - 1))
-                    cigar_last = 3
+                    new_read_cigar_tuple_list.append((BAM_CREF_SKIP, position_delta - 1))
+                    cigar_last = BAM_CREF_SKIP
                     cigar_last_count = position_delta
 
             qs = pileupcolumn.get_query_sequences()
@@ -123,23 +133,21 @@ def call_consensus(output_bam, temp_bam_filename, new_read_name=None, temp_sorte
             called_base, called_quality, called_cigar = call_base(qs, qq)
 
             if called_base == "":
-                if cigar_last == 3:
+                if cigar_last == BAM_CREF_SKIP:
                     cigar_last_count += 1
                 else:
                     new_read_cigar_tuple_list.append((cigar_last, cigar_last_count))
-                    # N BAM_CREF_SKIP 3
-                    new_read_cigar_tuple_list.append((3, position_delta - 1))
-                    cigar_last = 3
+                    new_read_cigar_tuple_list.append((BAM_CREF_SKIP, position_delta - 1))
+                    cigar_last = BAM_CREF_SKIP
                     cigar_last_count = position_delta
             else:
                 new_read_sequence_list.append(called_base)
                 new_read_quality_list.append(called_quality)
 
-                # M BAM_CMATCH 0
-                if cigar_last == 0:
+                if cigar_last == BAM_CMATCH:
                     cigar_last_count += 1
                 else:
-                    cigar_last = 0
+                    cigar_last = BAM_CMATCH
                     cigar_last_count = 1
 
             last_pileup_position = pos
@@ -148,29 +156,31 @@ def call_consensus(output_bam, temp_bam_filename, new_read_name=None, temp_sorte
         new_read_cigar_tuple_list.append((cigar_last, cigar_last_count))
 
     # Construct new AlignedSegment
+    quality_string = ''.join(new_read_quality_list)
+    quality_array = pysam.qualitystring_to_array(quality_string)
+
     nr = pysam.AlignedSegment()
     nr.query_name = new_read_name
     nr.query_sequence = ''.join(new_read_sequence_list)
-    nr.flag = 0  # TODO
+    # TODO: Set the flags correctly
+    nr.flag = 0
     nr.reference_id = reference_id
     nr.reference_start = first_pileup_position
     nr.mapping_quality = 255
-    #print()
-    #print(new_read_name)
-    #print(nr.query_sequence)
-    #print(len(nr.query_sequence))
-    #print(new_read_cigar_tuple_list)
-    quality_string = ''.join(new_read_quality_list)
-    #print(quality_string)
-    quality_array = pysam.qualitystring_to_array(quality_string)
-    #print(quality_array)
-
     nr.cigartuples = new_read_cigar_tuple_list
     nr.query_qualities = quality_array
     nr.tags = tags
 
-    # Write it to the output file
-    output_bam.write(nr)
+    if debug:
+        print('---')
+        print(new_read_name)
+        print(nr.query_sequence)
+        print(len(nr.query_sequence))
+        print(new_read_cigar_tuple_list)
+        print(quality_string)
+        print(quality_array)
+
+    return nr
 
 
 def umi_collapse(input_file, output_file, verbose=False, input_is_sorted=False, debug=False):
@@ -189,7 +199,8 @@ def umi_collapse(input_file, output_file, verbose=False, input_is_sorted=False, 
     # Do the collapse
     print('Collapsing reads...')
     umi_collapse_sorted_file(sorted_input_filename, output_file, verbose=verbose,
-                             total_reads=total_number_of_reads, debug=debug)
+                             total_reads=total_number_of_reads, debug=debug,
+                             save_collapsed_read=True)
 
     print('Cleaning up...')
     if not input_is_sorted and not debug:

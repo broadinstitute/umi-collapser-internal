7748e9edb8bd33ad582020ce1eb3ce98433c403a
Nikolas Barkas <nikolas.barkas@outlook.com>
2020-06-02 14:08:05 -0400

    working on setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="umi_collapser",
+    name="umi-collapser",
+    version="0.0.1",
     author="Nikolas Barkas",
     author_email="nbarkas@broadinstitute.org",
     description="scRNA-seq UMI read collapser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/broadinstitute/umi-collapser-internal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         "pysam",
         "tqdm",
@@ -14,7 +24,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'umiCollapser=umi_collapser.__main__:main'
+            'umiCollapser=umi-collapser.__main__:main'
         ]
     }
 )
